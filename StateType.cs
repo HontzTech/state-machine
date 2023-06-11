a69141358c381105333673318eeb891fc6725b6e
Håkon Hesselberg Hålu <hakon.hesselberg@gmail.com>
2023-06-11 22:57:34 +0200

    Create StateType.cs

---
+++ StateType.cs
@@ -0,0 +1,15 @@
+namespace ArcaneHeist.Scripts.EnemyBehaviour
+{
+    /// <summary>
+    /// Enum representing the possible types of states.
+    /// </summary>
+    public enum StateType
+    {
+        Patrolling,
+        Discovering,
+        Chasing,
+        Scanning,
+        Idle,
+        Triggered
+    }
+}
